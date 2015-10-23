b818e15853207def9f0e259b2a3c72fa3ed4a4d4
DeAndre Johnson <dre@phreshr.com>
2015-10-22 23:13:54 -0400

    added back social buttons component

--- src/app/components/social-buttons/social-buttons.js
+++ src/app/components/social-buttons/social-buttons.js
@@ -36,20 +36,6 @@ class SocialButtons {
    * Handles on load processing, and loading initial data
  */
   activate() {
-    this.openService = (service) => {
-      switch (service) {
-        case 'twitter':
-          this.$window.open('http://twitter.com/share?text=' + this.title, 'twitter-share', 'width=550,height=235');
-          break;
-        case 'facebook':
-          this.$window.open('https://www.facebook.com/sharer/sharer.php', 'facebook-share', 'width=580,height=296');
-          break;
-        case 'google':
-          this.$window.open('https://plus.google.com/share', 'google-plus-share', 'width=490,height=530');
-          break;
-        // no default
-      }
-    };
 
     this.activated = true;
   }
